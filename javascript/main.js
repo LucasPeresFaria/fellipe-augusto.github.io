1e9571bae84a45bb3b6980d017e865cf27f30aa4
Fellipe Augusto <34405426+fellipe-augusto@users.noreply.github.com>
2019-04-16 15:30:09 -0300

    Update
    - Corrigido alguns erros
    - Adicionado section Hobby
    - Adicionado SlideShow

---
+++ javascript/main.js
@@ -0,0 +1,24 @@
+// Funções by https://www.w3schools.com/howto/howto_js_slideshow.asp
+// Adaptado por Fellipe Augusto
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("hobbySlide");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
